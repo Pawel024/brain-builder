025e5a0297152696edc72360886567b2e418fc18
Pawel024 <p.j.rok@student.tudelft.nl>
2023-09-22 22:44:31 +0200

    version 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const messages = [
     'Strange women lying in ponds distributing swords is no basis for a system of government.',
@@ -13,8 +15,17 @@ function App() {
   ];
 
   const generateMessage = () => {
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    setMessage(messages[randomIndex]);
+    setIsLoading(true);
+    setProgress(0);
+    const interval = setInterval(() => {
+      setProgress(progress => progress + 10);
+    }, 200);
+    setTimeout(() => {
+      clearInterval(interval);
+      const randomIndex = Math.floor(Math.random() * messages.length);
+      setMessage(messages[randomIndex]);
+      setIsLoading(false);
+    }, 2000);
   };
 
   return (
@@ -24,9 +35,15 @@ function App() {
           <button onClick={generateMessage} style={{ backgroundColor: 'darkred', color: 'white', fontSize: '24px', textAlign: 'center', transition: 'background-color 0.5s ease', padding: '10px 20px', borderRadius: '5px', marginBottom: '20px', marginTop: '40px' }}>
             Click me!
           </button>
-          <div style={{ color: 'black', fontFamily: 'Arial', fontSize: '18px', textAlign: 'center', fontWeight: 'bold' }}>
-            {message}
-          </div>
+          {isLoading ? (
+            <div style={{ width: '80%', height: '30px', borderRadius: '5px', backgroundColor: 'white' }}>
+              <div style={{ width: `${progress}%`, height: '100%', backgroundColor: 'darkred', borderRadius: '5px' }}></div>
+            </div>
+          ) : (
+            <div style={{ color: 'black', fontFamily: 'Arial', fontSize: '18px', textAlign: 'center', fontWeight: 'bold' }}>
+              {message}
+            </div>
+          )}
         </div>
       </div>
     </div>
