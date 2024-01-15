58b62f735f27272cbbf8fdf360c114171f2b6d84
Pawel024 <p.j.rok@student.tudelft.nl>
2024-01-15 04:18:46 +0100

    stop index from overwriting the api

--- django_react_proj/urls.py
+++ django_react_proj/urls.py
@@ -21,9 +21,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('<path:path>', views.index),
     re_path(r'^api/backend$', views.query_list),
     re_path(r'^api/backend/$', views.query_list),
     re_path(r'^api/backend/(?P<pk>[0-9]+)$', views.query_detail),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+
+    # catch-all pattern
+    re_path(r'^.*$', views.index),
 ]
