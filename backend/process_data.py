2eded8907794ed3c8e4f00982ef21f8093f0747a
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-15 19:35:49 +0100

    WARNING: this will intentionally break, to see how far we can get in process_data.py

--- backend/process_data.py
+++ backend/process_data.py
@@ -23,9 +23,11 @@ import pandas as pd
 
 
 def process(req, root_link, pk=None):
+    assert pk is not None, "no pk"
     req = dict(req)
     task_id, user_id = req['task_id'], req['user_id']
-    requests.put(root_link + 'api/progress/' + str(pk) + '/', json={'progress': 1, 'plots': json.dumps([]), 'error_list': json.dumps([]), 'user_id': user_id, 'task_id': task_id})
+    response = requests.put(root_link + 'api/progress/' + str(pk) + '/', json={'progress': 1, 'plots': json.dumps([]), 'error_list': json.dumps([]), 'user_id': user_id, 'task_id': task_id})
+    assert False, response
 
     # load the games dataframe from the API
     #  this dataframe contains all the game-specific info the backend uses
