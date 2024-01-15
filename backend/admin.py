ffcf78dd15c37b755e8dc35b2128d01df1789876
Pawel024 <p.j.rok@student.tudelft.nl>
2024-01-15 16:57:04 +0100

    add progress to admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from .models import Row, TaskDescription
+from .models import Row, TaskDescription, Progress
 
 admin.site.register(Row)
 admin.site.register(TaskDescription)
+admin.site.register(Progress)
