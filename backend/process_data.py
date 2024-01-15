75dfef970c6d7cebe6a0bf56fe133ba40764c933
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-15 19:53:34 +0100

    Let's try with post instead of put

--- backend/process_data.py
+++ backend/process_data.py
@@ -23,10 +23,9 @@ import pandas as pd
 
 
 def process(req, root_link, pk=None):
-    assert pk is not None, "no pk"
     req = dict(req)
     task_id, user_id = req['task_id'], req['user_id']
-    response = requests.put(root_link + 'api/progress/' + str(pk) + '/', json={'progress': 1, 'plots': json.dumps([]), 'error_list': json.dumps([]), 'user_id': user_id, 'task_id': task_id})
+    response = requests.post(root_link + 'api/progress/', json={'progress': 1, 'plots': json.dumps([]), 'error_list': json.dumps([]), 'user_id': user_id, 'task_id': task_id})
     assert False, response
 
     # load the games dataframe from the API
