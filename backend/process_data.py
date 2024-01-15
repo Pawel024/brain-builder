280c0c577750715557ffd844890075262de1266d
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-15 20:58:12 +0100

    Code breaks in process_data.py on line 29, let's see if lines 39-42 work and have a look at the request

--- backend/process_data.py
+++ backend/process_data.py
@@ -25,7 +25,8 @@ import pandas as pd
 def process(req, root_link, pk=None):
     req = dict(req)
     task_id, user_id = req['task_id'], req['user_id']
-    assert False, root_link
+
+    """
     response = requests.post(root_link + 'api/progress/', json={'progress': 1, 'plots': json.dumps([]), 'error_list': json.dumps([]), 'user_id': user_id, 'task_id': task_id})
 
     # load the games dataframe from the API
@@ -33,10 +34,14 @@ def process(req, root_link, pk=None):
     response = requests.get(root_link + 'api/tasks/?user_id=' + str(req['user_id']) + '&task_id=' + str(req['task_id']))
     levels.games = response.json()
     levels.games = pd.DataFrame(levels.games)
+    """
+
     df.root_link, mn.root_link = root_link, root_link
     df.task_id, mn.task_id = task_id, task_id
     df.user_id, mn.user_id = user_id, user_id
     df.pk, mn.pk = pk, pk
+
+    assert False, req
 
 
     if req['action'] == 1:  # create and train a network
