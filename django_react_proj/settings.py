034342bbc30a4aa0b189425104dcdb10094d311b
Pawel024 <p.j.rok@student.tudelft.nl>
2024-01-15 01:01:50 +0100

    put MessageMiddleware back

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
