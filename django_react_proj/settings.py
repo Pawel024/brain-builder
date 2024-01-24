2e9c6c4ce199bd35c28cc553b3f45506f26d0c57
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-24 11:22:14 +0100

    Deleting django_grip

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_grip.GripMiddleware',
 ]
 
 redis_url = urllib.parse.urlparse(os.environ.get('REDIS_URL', 'redis://localhost:6379'))
