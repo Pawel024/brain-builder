a23f51a3d87fa040688b897b228f6a3b659613a0
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-24 05:30:10 +0100

    No middleware necessary

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_grip.GripMiddleware',
-    'django_eventstream.middleware.EventStreamMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
