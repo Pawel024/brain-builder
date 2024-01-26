8eadc4e430a768ec6d91a9e8f4fbcfebd7c38cf2
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-26 23:18:49 +0100

    Updating routing.py to work with UUIDs

--- django_react_proj/routing.py
+++ django_react_proj/routing.py
@@ -9,9 +9,29 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/(?P<userId>\w+)/(?P<taskId>\w+)/$', consumers.Coach.as_asgi()),
+    re_path(r'ws/(?P<userId>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/(?P<taskId>\w+)/$', consumers.Coach.as_asgi()),
     # Add more WebSocket endpoints here if needed
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
