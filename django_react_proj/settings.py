d7c699c05233e610da717bfbe928cbd5a9579576
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-27 00:03:59 +0100

    Adding authentication...

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -84,6 +84,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [(redis_url.hostname, redis_url.port)],
+            "password": redis_url.password,
         },
     },
 }
