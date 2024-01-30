d1ed30c538f87e10ad939328974c5a456425430d
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-01-30 02:42:49 +0100

    Changing the routing

--- django_react_proj/routing.py
+++ django_react_proj/routing.py
@@ -10,7 +10,7 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/(?P<userId>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/(?P<taskId>\w+)/$', consumers.Coach.as_asgi()),
-    re_path(r'ws/(?P<userId>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/custom(?P<customId>\w)/$', consumers.Plotter.as_asgi()),
+    re_path(r'custom/(?P<userId>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/(?P<customId>\w)/$', consumers.Plotter.as_asgi()),
     # Add more WebSocket endpoints here if needed
 ]
 
