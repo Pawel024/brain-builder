9e1754817c62415412aeb7818eea16d5af844215
Pawel024 <p.j.rok@student.tudelft.nl>
2024-01-31 04:23:28 +0100

    some visual improvements for custom blocks

--- src/customBlocks.js
+++ src/customBlocks.js
@@ -109,6 +109,8 @@ class CustomBlock extends Component {
 
         return (
         <Theme accentColor="cyan" grayColor="slate" panelBackground="solid" radius="large" appearance='light'>
+            <div className='App'>
+            <Flex direction='column' gap='0'>
             <Box py="2" style={{ backgroundColor: "var(--cyan-10)"}}>
             <Grid columns='3' mt='1'>
             <Box ml='3' style={{display:"flex"}}>  
@@ -125,14 +127,15 @@ class CustomBlock extends Component {
                 <Link to="https://www.tudelft.nl/en/" target="_blank" style={{ textDecoration: 'none'}}>
                     <img src={tu_delft_pic} alt='Tu Delft Logo' width='auto' height='30'/>
                 </Link>
-                </Box>
+            </Box>
             </Grid>
             </Box>
 
+            <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', fontFamily: 'monospace', height: window.innerHeight-52, width:'100vw' }}>
             {this.props.customId === 11 && (
-                <Flex direction='row' gap={0}>
-                    <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-                        <Flex direction='column' gap={2}>
+                <Flex direction='row' gap={0} style={{ height: window.innerHeight-52, width:'100vw', alignItems: 'center', justifyContent: 'center' }}>
+                    <Box style={{ flex:1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px' }}>
+                        <Flex direction='column' gap={2} style={{ alignItems: 'center', justifyContent: 'center' }}>
                         <div className="weightSlider">
                             {weightSlider}
                         </div>
@@ -144,15 +147,17 @@ class CustomBlock extends Component {
                         <img src={this.state.img} alt="No plot available" />
                         </Flex>
                     </Box>
-                    <Separator orientation='vertical' style = {{ height: '100vh' }}/>
-                    <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu</p>
+                    <Separator orientation='vertical' style = {{ height: window.innerHeight-100 }}/>
+                    <Box style={{ flex:1, display: 'flex', flexDirection: 'column', textAlign:'justify', alignItems: 'flex-start', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px' }}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae elementum curabitur vitae nunc sed. Consectetur purus ut faucibus pulvinar elementum integer enim. Diam quis enim lobortis scelerisque fermentum dui. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Consectetur adipiscing elit ut aliquam purus sit. At erat pellentesque adipiscing commodo. Aliquam etiam erat velit scelerisque in. Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Sed viverra tellus in hac. Eros donec ac odio tempor orci dapibus. Facilisi cras fermentum odio eu feugiat. Ut eu sem integer vitae justo eget.</p>
                     </Box>
                 </Flex>
             )}
 
             {/* Add more custom blocks later */}
-
+            </Box>
+            </Flex>
+            </div>
         </Theme>
         );
     }
