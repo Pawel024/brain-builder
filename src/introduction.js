3ade5dc85dc35352323bac561c397fdc40c84796
Pawel024 <p.j.rok@student.tudelft.nl>
2024-02-29 11:05:43 +0100

    Another attempt to highlight the current slide in the menu

--- src/introduction.js
+++ src/introduction.js
@@ -88,7 +88,7 @@ class Introduction extends React.Component {
               <Box style={{ flexBasis: '33%', padding: '0px 100px', display: 'flex', justifyContent:"center", alignItems:"center" }}>
                 <Flex direction="column" gap="2" style={{ justifyContent:"center", alignItems:"center", width:"100%" }}>
                   {this.state.content.map(([subtitle, text], index) => (
-                    <Button variant="outline" style={{ width:"100%" }} onClick={() => this.goToSlide(index)} pressed={this.state.currentSlide === index}>{subtitle}</Button>
+                    <Button variant="outline" style={{ width:"100%", backgroundColor: this.state.currentSlide === index ? 'var(--accent-a3)' : 'white'}} onClick={() => this.goToSlide(index)}>{subtitle}</Button>
                   ))}
                 </Flex>
               </Box>
