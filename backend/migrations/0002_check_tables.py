1f6c682b8b4fa47464e8a8cf6ddef509446792d8
Pawel024 <p.j.rok@student.tudelft.nl>
2024-03-03 22:59:24 +0100

    Database workaround

---
+++ backend/migrations/0002_check_tables.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            DO $$
+            BEGIN
+                IF NOT EXISTS (
+                    SELECT FROM information_schema.tables 
+                    WHERE  table_name   = 'backend_feedback'
+                ) THEN
+                    CREATE TABLE backend_feedback (
+                        id serial PRIMARY KEY,
+                        feedback jsonb NOT NULL,
+                        timestamp timestamp with time zone NOT NULL
+                    );
+                END IF;
+            END
+            $$;
+            """
+        ),
+    ]
