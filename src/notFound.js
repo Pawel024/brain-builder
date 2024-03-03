ba5eb3a6459e2274f3892149bf470be7fabfc67c
Pawel024 <p.j.rok@student.tudelft.nl>
2024-03-03 14:01:34 +0100

    Update background image in notFound.js

--- src/notFound.js
+++ src/notFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Heading } from '@radix-ui/themes';
+import montyPythonImage from './monty-python.jpeg';
 
 
 /**
@@ -11,13 +12,29 @@ function NotFound() {
     const isMontyPythonLover = true; // Replace with your condition
 
     return (
-        // disable both horizontal and vertical scrolling, cut off the overflow
-        <Box style={{ overflow: 'hidden', position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Box style={{ textAlign: 'center', color: 'white', zIndex: 999 }}>
-                <Heading style={{ fontSize:90 }}>404</Heading>
-                <p style={{ fontSize:90 }}>Page not found : ( </p>
+        <Box style={{ 
+            overflow: 'hidden', 
+            backgroundImage: `url(${isMontyPythonLover ? montyPythonImage : ''})`, // Set the image as the background
+            backgroundSize: 'cover', // Cover the entire area
+            backgroundPosition: 'top left', // Align the image to the top left
+        }}>
+            <Box style={{ 
+                position: 'fixed', 
+                top: 0, 
+                left: 0, 
+                width: '100%', 
+                height: '100%', 
+                display: 'flex', 
+                flexDirection: 'column', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                zIndex: 999 
+            }}>
+                <Box style={{ textAlign: 'center', color: 'white' }}>
+                    <Heading style={{ fontSize:90 }}>404</Heading>
+                    <p style={{ fontSize:48 }}>Page not found : ( </p>
+                </Box>
             </Box>
-            {isMontyPythonLover && <img src={require('./monty-python.jpeg')} alt="Monty Python" />}
         </Box>
     );
 }
