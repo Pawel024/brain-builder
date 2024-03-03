ffc0cf5a7f13399363592b03cc1706e973c1f41b
Pawel024 <p.j.rok@student.tudelft.nl>
2024-03-03 12:49:29 +0100

    FUCKING CURLY BRACKETS

--- src/readme.js
+++ src/readme.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 import './App.css';
 
-function Readme(file) {
+function Readme({ file }) {
   const [readme, setReadme] = useState('');
 
   useEffect(() => {
