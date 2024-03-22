63c45931fc55dd4ac2fbbdb2625efcbb80cce126
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-03-22 01:45:11 +0100

    Proper build

--- src/buildView.js
+++ src/buildView.js
@@ -331,10 +331,12 @@ class Building extends React.Component {
   }
 
   handleStartClick = (event) => {
+    console.log("Click detected")
     if (this.props.taskId !== 0) { if (this.props.isTraining === 1) 
       {this.props.cancelRequest(this.props.taskId, this.props.index)
       } else { this.props.putRequest(event, this.props.cytoLayers, this.props.apiData, this.props.setApiData, this.props.setAccuracy, this.props.setIsTraining, this.props.learningRate, this.props.iterations, this.props.taskId, this.props.index, this.props.nOfInputs, this.props.nOfOutputs, this.props.normalization, this.props.af)
       }
+      console.log("Click executed")
     }
   }
 
