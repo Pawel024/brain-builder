dcc8152c8cd6f18f13cad9481c274d2e4ed7cfed
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-03-26 18:17:25 +0100

    Bringing my code up to date

--- src/codePreview.js
+++ src/codePreview.js
@@ -13,6 +13,15 @@ import a11yDark from './a11y-dark';
  * @returns {JSX.Element} The JSX element representing the code preview.
  */
 
+const steps = [
+    {
+        target: '.CodePreview',
+        content: 'Here you can see the code that corresponds to the neural network you just built. You can copy and paste this code into your Python environment to train the model.',
+        disableBeacon: true,
+        placement: 'top',
+    },
+];
+
 function CodePreview({ code }) {
     return (
         <Box style={{ 
@@ -22,10 +31,11 @@ function CodePreview({ code }) {
             justifyContent: 'center', 
             alignItems: 'center'
         }}>
-            <Box style={{ textAlign: 'center', color: 'white', backgroundColor: 'transparent', width: window.innerWidth*0.97, height: window.innerHeight*0.97, borderRadius: "var(--radius-3)" }}>
-                <SyntaxHighlighter language="python" style={a11yDark}>
+            <Box style={{ position: 'relative', textAlign: 'center', color: 'white', backgroundColor: 'transparent', width: window.innerWidth*0.97, height: window.innerHeight*0.97, borderRadius: "var(--radius-3)" }}>
+                <SyntaxHighlighter language="javascript" style={a11yDark}>
                     {code}
                 </SyntaxHighlighter>
+                <Box classname="torch" style={{ position: 'absolute', top: 14, left: 12, width: 100, height: 21 }}></Box>
             </Box>
         </Box>
     );
