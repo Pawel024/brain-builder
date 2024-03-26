ad03aada0e6c289fca5eb3b3734876cadf550918
Pawel024 <p.j.rok@student.tudelft.nl>
2024-03-26 01:46:35 +0100

    maybe fix the width='auto'  issue

--- src/App.js
+++ src/App.js
@@ -1601,8 +1601,8 @@ function App() {
                 <Grid columns='3' mt='1'>
                   <Box ml='3' style={{display:"flex"}}>  
                     <Link to="/">
-                      <IconButton aria-label="navigate to home" width='auto' height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
-                        <HomeIcon color="white" width='auto' height='18' style={{ marginTop: 2 }} />
+                      <IconButton aria-label="navigate to home" height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
+                        <HomeIcon color="white" height='18' style={{ marginTop: 2 }} />
                       </IconButton>
                     </Link>
                   </Box>
@@ -1629,8 +1629,8 @@ function App() {
             <Grid columns='3' mt='1'>
               <Box ml='3' style={{display:"flex"}}>  
                 <Link to="/">
-                  <IconButton aria-label="navigate to home" width='auto' height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
-                    <HomeIcon color="white" width='auto' height='18' style={{ marginTop: 2 }} />
+                  <IconButton aria-label="navigate to home" height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
+                    <HomeIcon color="white" height='18' style={{ marginTop: 2 }} />
                   </IconButton>
                 </Link>
               </Box>
--- src/introduction.js
+++ src/introduction.js
@@ -104,8 +104,8 @@ class Introduction extends React.Component {
           <Grid columns='3' mt='1'>
           <Box ml='3' style={{display:"flex"}}>  
               <Link to="/">
-                  <IconButton aria-label="navigate to home" width='auto' height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
-                  <HomeIcon color="white" width='auto' height='18' style={{ marginTop: 2 }} />
+                  <IconButton aria-label="navigate to home" height='21' style={{ marginLeft: 'auto', color: 'inherit', textDecoration: 'none' }}>
+                  <HomeIcon color="white" height='18' style={{ marginTop: 2 }} />
                   </IconButton>
               </Link>
               </Box>
